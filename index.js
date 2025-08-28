61d6479163dd5dbe5e75cf33d8448d2285e686e6
Calderon <jacalderon@escl.gt>
2025-08-28 08:15:20 -0600

    arreglo cors

--- index.js
+++ index.js
@@ -10,7 +10,9 @@ const app = express();
 
 //CORS
 app.use(cors({
-    origin: ['http://127.0.0.1:5500'],
+    origin: ['http://127.0.0.1:5500',
+        'https://andreecalderon.github.io'
+    ],
     methods: ['GET', 'POST', 'OPTIONS'],
     allowedHeaders:['COontent-Type', 'Authorization']
 }))
